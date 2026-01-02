e51bd31d67b1008a5bf5b3d0ef3e7025ed20cae5
Aun-Ali-Kazmi <syedaunalikazmi786@gmail.com>
2026-01-03 00:16:01 +0100

    Day 2

---
+++ Basics.py
@@ -0,0 +1,5 @@
+#Just a basiic code for getting monthhly Salary and Savings per month
+yearly_salary = int(input("Enter your Yearly salary"))
+print("Monthly Salary =", round(yearly_salary/12))
+savings = yearly_salary*0.2
+print(f"Savings = {savings} and it is good")
