25dd8cb88c6c828200c581f70ef3a63480a6b2d7
Aun-Ali-Kazmi <syedaunalikazmi786@gmail.com>
2026-01-03 00:59:20 +0100

    Day 2: Conditions and loops in Python

--- Basics.py
+++ Basics.py
@@ -1,11 +1,9 @@
-#Vriables, Print and Input
 
-#Just a basic code for getting monthhly Salary and Savings per month
-yearly_salary = int(input("Enter your Yearly salary")) #input always string
-savings = yearly_salary*0.2
-Monthly_salary = round(yearly_salary/12)
-
-print(f"Monthly Salary = {Monthly_salary}")
-print(f"Yearly Savings = {savings}")
-
-#
+#Conditions and Loops
+6
+num = int(input("Enter any number = "))
+print("Even Numbers")
+for i in range(1,num+1):
+    if i % 2==0:
+        print(i)
+    
